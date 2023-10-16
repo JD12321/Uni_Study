466db106ffb0e5f9b1cf065ec74cd12be9a563c4
EUN JIDAM <127118453+JD12321@users.noreply.github.com>
2023-10-16 11:08:11 +0900

    Update 01.java

--- 자바프로그래밍/Week02/01.java
+++ 자바프로그래밍/Week02/01.java
@@ -20,24 +20,3 @@ public class TwoDice {
 	}
 
 }
-
-
-
-"""
-2.
-"""
-package homework;
-
-import java.util.*;
-
-public class Wizard {
-	public static void main(String[] args) {
-		Random r = new Random();
-		
-		int x = r.nextInt(11) + 20;
-		
-		System.out.println("마법사가 " + x + "의 공격으로 적을 공격했습니다.");
-		
-	}
-
-}
