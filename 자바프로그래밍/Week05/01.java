2a099abfb90514bd6181eb79b68fd415a2e743fa
EUN JIDAM <127118453+JD12321@users.noreply.github.com>
2023-10-17 09:03:14 +0900

    Create 자바프로그래밍/Week05/01.java

---
+++ 자바프로그래밍/Week05/01.java
@@ -0,0 +1,15 @@
+"""
+1.
+"""
+package homework01;
+
+public class ch101 {
+	public static void main(String[] args) {
+
+		int total = 0;
+		for (int i = 0; i < 101; i++) {
+			total = total + i;
+		}
+		System.out.println(total);
+	}
+}
