b2b7261e990f0ada4164a8fc4d2eaec2ea351515
EUN JIDAM <127118453+JD12321@users.noreply.github.com>
2023-10-18 16:06:22 +0900

    Create exam_1013_01.py

---
+++ 데이터분석입문/Week06/exam_1013_01.py
@@ -0,0 +1,11 @@
+"""
+exam_1013_01.py
+"""
+
+from exam_1013_01_data import get_temp_data
+from exam_1013_01_data import display_chart
+
+
+temp_data = get_temp_data()
+print(temp_data)
+display_chart(temp_data)
