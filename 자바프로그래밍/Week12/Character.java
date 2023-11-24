324cc97e01567b60a3b69092c4ea92229dd7abf9
EUN JIDAM <127118453+JD12321@users.noreply.github.com>
2023-11-24 22:07:47 +0900

    Update Character.java

--- 자바프로그래밍/Week12/Character.java
+++ 자바프로그래밍/Week12/Character.java
@@ -1,14 +1,20 @@
 package fighter;
 
-import java.util.*;
+import java.util.Random;
 
 public class Character {
+	
 	Random r = new Random();
-
 	String name;
 	private int hp;
 	int max;
 	
+	public Character(String n, int h, int m) {
+		name = n;
+		hp = h;
+		max = m;
+	}
+
 	public int getHp() {
 		return hp;
 	}
@@ -16,23 +22,22 @@ public class Character {
 	public void setHp(int hp) {
 		this.hp = hp;
 	}
-
-	public Character(String name, int hp, int max) {
-		this.name = name;
-		this.hp = hp;
-		this.max = max;
+	
+	public boolean attack(Character enemy) {
+		
+		if(enemy instanceof Boss) {
+			Boss b = (Boss)enemy; //다운캐스팅
+			b.setHp(b.getHp() - (r.nextInt(max) + 1));
+		} else if (enemy instanceof Character) {
+			Character c = (Character)enemy;
+			c.setHp(c.getHp() - (r.nextInt(max) + 1));
+		}
+		
+		System.out.println(enemy.name+"의 현재 hp는 " + enemy.getHp() + "입니다.");
+		if (enemy.hp<=0) {
+			System.out.println(enemy.name + " 쓰러졌습니다. 게임을 종료합니다.");
+			return false;
+		} else return true;
 	}
-
-	public int attack(Character enemy) {
-		enemy.setHp(enemy.getHp() - r.nextInt(max) + 1);
-		return power;
-	}
-
-	public int dps(int a) {
-		if (a == 1)
-			return 0;
-		else
-			return 1;
-	}
-
+	
 }
