a724b622cde5ad644ac1b02e2c6ab05d5e47f558
EUN JIDAM <127118453+JD12321@users.noreply.github.com>
2023-11-24 22:07:33 +0900

    Update FightGame.java

--- 자바프로그래밍/Week12/FightGame.java
+++ 자바프로그래밍/Week12/FightGame.java
@@ -1,42 +1,20 @@
 package fighter;
 
-import java.util.*;
-
 public class FightGame {
 	public static void main(String[] args) {
-		Random r = new Random();
-
-		Character me = new Character("은지담", 200, 15);
-		Boss boss = new Boss("보스", 300, 20);
-
-		while (true) {
-			int mc = r.nextInt(2);
-			if (mc == 0) {
-				me.attack(boss);
-				System.out.println(me.name + "의 공격: " + boss.name + "의 현재 hp는 " + boss.hp + "입니다.");
-			} else
-				System.out.println(me.name + "의 공격 방어");
-			if (boss.hp < 0) {
-				System.out.println(boss.name + "이(가) 쓰러졌습니다. 게임을 종료합니다.");
+		
+		Character me = new Character("은지담", 100, 20);
+		Boss boss = new Boss("보스", 200, 10);
+		
+		while(true) {
+			boolean kenAlive = me.attack(boss);
+			if(kenAlive==false) {
 				break;
 			}
-
-			int bc = r.nextInt(5);
-			if (bc == 0)
-				setHp() -= me.dps(mc) * boss.psg();
-			
-				setHp
-				getHp
-			else
-				me.hp -= me.dps(mc) * boss.Battack();
-
-			System.out.println(boss.name + "의 공격: " + me.name + "의 현재 hp는 " + me.hp + "입니다.");
-
-			if (me.hp < 0) {
-				System.out.println(me.name + "이(가) 쓰러졌습니다. 게임을 종료합니다.");
+			boolean ryuAlive = boss.attack(me);
+			if(ryuAlive==false) {
 				break;
 			}
-
 		}
 	}
 }
